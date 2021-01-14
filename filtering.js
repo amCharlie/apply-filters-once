7751778f40ffb44396e0176b1109e41a136e9ad8
charliewu <1075318919@qq.com>
2021-01-14 15:06:17 +0800

    提交

--- filtering.js
+++ filtering.js
@@ -5,81 +5,90 @@
 	let unregisterHandlerFunctions = [];
 	
   $(document).ready(function () {
+	$('#loading').removeClass('hidden').addClass('hidden');
 	tableau.extensions.initializeAsync()
 	$('#apply').click(applyAllFilters);  
   });
 
   // apply all selected filters
   function applyAllFilters () {
-	alert("数据刷新中...");
+	$('#loading').removeClass('hidden').addClass('show');
    // alert("apply all filters!")
    // Initialization succeeded! Get the dashboard
    let dashboard = tableau.extensions.dashboardContent.dashboard;
-   
    let filterFetchPromises = [];
    
-   let record_filter_applied_values = {}
-   
+   let worksheetFiltersPromiss = [];
+   let worksheet_dict = {}
    dashboard.worksheets.forEach(function (worksheet) {
-      worksheet.getFiltersAsync().then(function (filtersForWorksheet) {
-        filtersForWorksheet.forEach(function (filter) {
-			var f_name = filter.fieldName;
-			// if (f_name == 'BG' || f_name == 'k_BG'){
-			//		alert('读取到筛选器：' + f_name);
-			//}
-			var all_selected = filter.isAllSelected; 
-			var f_type = filter.filterType;
-			var vals = filter.appliedValues;
-			var vals_array = [];
-	
-			if (f_name.substr(0, 2) == 'k_') {
-				try {
-					var len = vals.length;
-					// alert(len);
-					if (len > 0 || (len == 0 && all_selected == false)){
-						for (var i = 0; i < vals.length; i++) {
-							var v = vals[i].formattedValue;
-							vals_array.push(v);
-						}
-						
-					   dashboard.worksheets.forEach(function (worksheet_1) {
-						worksheet_1.getFiltersAsync().then(function (filtersForWorksheet_1) {
-							filtersForWorksheet_1.forEach(function (filter_1) {
-								var f_name_1 = filter_1.fieldName;
-								if (f_name.substring(2,) == f_name_1){
-									// if (f_name == 'BG' || f_name == 'k_BG'){
-									// 	alert("将" + f_name + "的筛选值应用于" + f_name_1);
-									// }
-									worksheet_1.applyFilterAsync(f_name_1, vals_array, 'replace', false);
-								}
-							});
-						});
-					   });
-				  } else if (len == 0 && all_selected == true) {
-					  dashboard.worksheets.forEach(function (worksheet_1) {
-						worksheet_1.getFiltersAsync().then(function (filtersForWorksheet_1) {
-							filtersForWorksheet_1.forEach(function (filter_1) {
-								var f_name_1 = filter_1.fieldName;
-								if (f_name.substring(2,) == f_name_1){
-									// if (f_name == 'BG' || f_name == 'k_BG'){
-									// 	alert("将" + f_name + "的筛选值应用于" + f_name_1);
-									// }
-									worksheet_1.applyFilterAsync(f_name_1, vals_array, 'all', false);
-								}
-							});
-						});
-					   });
-				  }
-				} catch(err) {
-					alert("error");
-				}
-			}
-        });
+	   worksheet_dict[worksheet.name] = worksheet;
+	   worksheetFiltersPromiss.push(worksheet.getFiltersAsync());
+   });
+   
+   
+   Promise.all(worksheetFiltersPromiss).then(function (fetchResults) {
+	   let k_filters = [];
+		fetchResults.forEach(function (worksheetFilters) {
+		  worksheetFilters.forEach(function (filter) {
+			  var f_name = filter.fieldName;
+			  if (f_name.substr(0, 2) == 'k_') {
+				  // alert(f_name);
+				  k_filters.push(filter);
+			  }
+		  });
+		});
 		
-      });
-    });
-	
-	alert("数据刷新完成！");
+		 // 封装刷新参数
+		  var worksheet_list = [];
+		  var apply_filter_list = [];
+		  var apply_values_list = [];
+		  var apply_type_list = [];
+		  
+		  for (var i = 0; i < k_filters.length; i++) {
+			  var filter = k_filters[i];
+			  var f_name = filter.fieldName.substring(2,);
+			  var all_selected = filter.isAllSelected; 
+			  var f_type = filter.filterType;
+			  var vals = filter.appliedValues;
+			  var vals_array = [];
+			  var apply_type = "";
+			  var len = vals.length;
+			  if (len > 0 || (len == 0 && all_selected == false)){
+				  for (var j = 0; j < vals.length; j++) {
+						var v = vals[j].formattedValue;
+						vals_array.push(v);
+				  }
+				  apply_type = 'replace';
+			  } else if(len == 0 && all_selected == true) {
+				  apply_type = 'all';
+			  }
+			  
+			  fetchResults.forEach(function (worksheetFilters) {
+				  let k_filters = [];
+				  worksheetFilters.forEach(function (filter) {
+					  var f_name_1 = filter.fieldName;
+					  if (f_name_1 == f_name) {
+						  var worksheet = worksheet_dict[filter.worksheetName];
+						  worksheet_list.push(worksheet);
+						  apply_filter_list.push(f_name_1);
+						  apply_values_list.push(vals_array);
+						  apply_type_list.push(apply_type);
+					  }
+				  });
+			  });
+		  }
+		  
+		  let apply_all_list = [];
+		  for (var i = 0; i < worksheet_list.length; i++) {
+			  apply_all_list.push(worksheet_list[i].applyFilterAsync(apply_filter_list[i], apply_values_list[i], apply_type_list[i], false));
+		  }
+
+			Promise.all(apply_all_list).then(function (){
+				 $('#loading').removeClass('hidden').addClass('hidden');
+			});
+		
+	  });
+  
   }
   
 })();
